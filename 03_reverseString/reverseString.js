52f9d0f1185cabfc878b2a3bac8ea9edf50ca17a
Yeidy Levels <ylevels@schools.nyc.gov>
2022-08-22 11:41:36 -0400

    reverseString() can reverse single word!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let original = string;
+    let reverse = "";
+
+    for (i = string.length - 1; i>=0; i--) {
+        reverse += original.slice(i, i+1);
+    }
+
+    return reverse;
 
 };
 
