db54c1832e487350e3dc59f8309fb71f2e2da882
Yeidy Levels <ylevels@schools.nyc.gov>
2022-08-22 10:26:12 -0400

    oops - fixed it to follow the directions!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
 const repeatString = function(string, num) {
+let result = "";
+let i = 0;
 
-    return string.repeat(num);
+while (i < num) {
+    result += string;
+    i++;
+}
+    return result;
 };
 
 // Do not edit below this line
