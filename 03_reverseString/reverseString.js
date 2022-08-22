dc465c66f178c470f7a09e2e0c29e4821aef19b3
Yeidy Levels <ylevels@schools.nyc.gov>
2022-08-22 11:44:56 -0400

    cleaned up unnecessary variable

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,8 @@
 const reverseString = function(string) {
-    let original = string;
     let reverse = "";
 
     for (i = string.length - 1; i>=0; i--) {
-        reverse += original.slice(i, i+1);
+        reverse += string.slice(i, i+1);
     }
 
     return reverse;
