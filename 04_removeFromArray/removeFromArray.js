220784572260e8a9b518f472742946d37cdd5be9
Yeidy Levels <ylevels@schools.nyc.gov>
2022-08-22 22:33:45 -0400

    finished two exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...theArgs) {
+    for (const arg of theArgs) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === arg) {
+                array.splice(i,1);
+            }
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,36 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    let array = [];
+    
+    if ( !Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0 ) {
+        return "ERROR"
+    }
+    
+    if (a < b) {
+        let length = b-a+1;
+        for (let i = 0; i < length; i++) {
+            array.push(a);
+            a++;
+        }
+        let sum = array.reduce(function (previousValue, currentValue) {
+            return previousValue + currentValue;
+        });
+        return sum;
+    //return array;
+    }
+
+    else {
+        let length = a-b+1;
+        for (let i = 0; i < length; i++) {
+            array.push(b);
+            b++;
+        }
+        let sum = array.reduce(function (previousValue, currentValue) {
+            return previousValue + currentValue;
+        });
+        return sum;
+    //return array;
+    }
 
 };
 
